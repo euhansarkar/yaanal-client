aabb8ae4c2e04566d664facce2d56d2d204c8f07
euhansarkar <euhansarkar@gmail.com>
2023-01-05 04:23:07 +0600

    basic tab added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import PopularProducts from '../PopularProducts/PopularProducts';
+import FeaturedFoods from '../FeaturedFoods/FeaturedFoods';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <PopularProducts></PopularProducts>
+            <FeaturedFoods></FeaturedFoods>
         </div>
     );
 };
